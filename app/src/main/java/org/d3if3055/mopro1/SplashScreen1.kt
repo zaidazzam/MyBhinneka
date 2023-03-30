f148b26f28f39e4e0af7126f07fb68dba55d7fc4
azzamzaid10@gmail.com <azzamzaid10@gmail.com>
2023-03-30 19:07:20 +0700

    2. Menambahkan SplashScreen1, SplashScreen2 & color

--- app/src/main/java/org/d3if3055/mopro1/SplashScreen1.kt
+++ app/src/main/java/org/d3if3055/mopro1/SplashScreen1.kt
@@ -1,15 +1,17 @@
 package org.d3if3055.mopro1
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.d3if3055.mopro1.databinding.ActivityMainBinding
+import org.d3if3055.mopro1.databinding.ActivitySplashScreen1Binding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class SplashScreen1 : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreen1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // inisiasi view binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySplashScreen1Binding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/org/d3if3055/mopro1/SplashScreen2.kt
@@ -0,0 +1,11 @@
+package org.d3if3055.mopro1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen2)
+    }
+}
